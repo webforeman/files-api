d0e5ae26b8a4372a15e62489dc08ac0968ced462
wf <webforeman@gmail.com>
2024-05-13 00:50:23 +0300

    fix: host env variable name

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 export const pool = new Pool({
   user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
+  host: process.env.POSTGRES_URL,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: parseInt(process.env.POSTGRES_PORT || '5432'),
