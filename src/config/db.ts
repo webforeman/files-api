323d2744854ba3b9fc565aa6070e561bfb80cbb5
wf <webforeman@gmail.com>
2024-05-13 19:33:09 +0300

    fix: host db in backend docker set equal compose service name

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,6 +5,7 @@ dotenv.config()
 
 export const pool = new Pool({
   user: process.env.POSTGRES_USER,
+  host: 'db',
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: parseInt(process.env.POSTGRES_PORT || '5432'),
