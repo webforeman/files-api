3af67264c3a605572b45be78ac769feb8e001ca6
wf <webforeman@gmail.com>
2024-05-14 04:34:18 +0300

    fix: db config for localhost and docker

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 export const pool = new Pool({
   user: process.env.POSTGRES_USER,
-  host: 'db',
+  host: process.env.NODE_ENV === 'production' ? 'db' : 'localhost',
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: parseInt(process.env.POSTGRES_PORT || '5432'),
